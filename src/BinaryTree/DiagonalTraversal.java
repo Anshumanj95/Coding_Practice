af6f4e30de6df24f52e31bf2e256e7455aa70054
Anshuman Jain <anshumanj95@gmail.com>
2022-07-06 20:13:33 +0530

    "7 July"

---
+++ src/BinaryTree/DiagonalTraversal.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+import static BinaryTree.Tree.Node;
+public class DiagonalTraversal {
+    public static ArrayList<Integer> diagonal(Node root){
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<Node> q=new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            Node curr=q.poll();
+            while (curr!=null){
+                if (curr.left!=null)
+                    q.add(curr);
+                ans.add(curr.key);
+                curr=curr.right;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
