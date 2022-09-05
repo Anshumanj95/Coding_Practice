5e4634a7c2871a20036656a676fd6755245b93c6
Anshuman Jain <anshumanj95@gmail.com>
2022-09-05 22:08:39 +0530

    "5 sept"

--- src/String/TransFiguration.java
+++ src/String/TransFiguration.java
@@ -11,6 +11,7 @@ public class TransFiguration {
             if (x!=0)
                 return -1;
         }
+        // if operation is to take any charter at index i and then add it at front
         int i=A.length()-1;
         int j=B.length()-1;
         int count=0;
@@ -22,6 +23,32 @@ public class TransFiguration {
             else {
                 count++;
                 i--;
+            }
+        }
+        return count;
+    }
+    public static int MinimumCostToConvert(String A,String B){
+        int[] freq=new int[256];
+        for (char ch:A.toCharArray())
+            freq[ch-'0']++;
+        for (char ch:B.toCharArray())
+            freq[ch-'0']--;
+        for (int x:freq){
+            if (x!=0)
+                return -1;
+        }
+        // if operation is to take any charter at index i and then add it at end
+        int i=0;
+        int j=0;
+        int count=0;
+        while (i<A.length() && j<B.length()){
+            if (A.charAt(i)==B.charAt(j)) {
+                i++;
+                j++;
+            }
+            else {
+                count++;
+                i++;
             }
         }
         return count;
