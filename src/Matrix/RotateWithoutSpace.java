680fd60908cf508cbcd6b505c2393db8d63ae5fd
Anshuman Jain <anshumanj95@gmail.com>
2022-09-11 21:50:37 +0530

    "11 sept"

--- src/Matrix/RotateWithoutSpace.java
+++ src/Matrix/RotateWithoutSpace.java
@@ -30,6 +30,29 @@ public class RotateWithoutSpace {
             }
         }
     }
+    public static  void anti_rotate90(int[][] matrix,int n){
+        //swap acc to secondary diagonal
+        for(int i=0;i<n-1;i++){
+            for (int j=0;j<n-1-i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][n-1-i];
+                matrix[n-1-j][n-1-i] = temp;
+            }
+        }
+        //swap acc to mid column
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
+        for (int[] arr:matrix){
+            for (int i:arr)
+                System.out.print(i+" ");
+            System.out.println();
+        }
+    }
     //first swap element according to main diagonal and then swap element according to middle column
     public static  void rotate90(int[][] matrix,int n){
         //swap acc to main diagonal
